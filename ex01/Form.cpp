7c7a02288dc2df5aaeff43a0d3a0d729c98268f5
Desausag <dinar095@hotmail.com>
2021-09-28 10:17:32 +0300

    ex01 ip progress

--- ex01/Form.cpp
+++ ex01/Form.cpp
@@ -1,5 +1,52 @@
-//
-// Created by Deckard Sausages on 9/27/21.
-//
-
 #include "Form.hpp"
+
+
+Form::Form(string name, int signgrade, int execgrade) : name(name), signGrade(signgrade), execGrade(execgrade), sign(false)
+{
+
+}
+
+string const Form::getName() const
+{
+	return name;
+}
+
+bool Form::getSign() const
+{
+	return sign;
+}
+
+int const Form::getSignGrade() const
+{
+	return signGrade;
+}
+
+int const Form::getExecGrade() const
+{
+	return execGrade;
+}
+
+void Form::beSigned(const Bureaucrat &src)
+{
+	if (src.getGrade() > signGrade)
+		throw Form::GradeTooLowException();
+	else
+		sign = true;
+}
+
+const char *Form::GradeTooHighException::what() const throw()
+{
+	return ("Grade is too high to sign this Form");
+}
+
+const char *Form::GradeTooLowException::what() const throw()
+{
+	return ("Grade is too low to sign this Form");
+}
+
+std::ostream& operator<<(std::ostream &out, Form const &src)
+{
+	out << "Form: " << src.getName() << " Sign: " << src.getSign() << " signGrade: ";
+	out << src.getSignGrade() << " execGrade: " << src.getExecGrade();
+	return out;
+}
--- ex01/main.cpp
+++ ex01/main.cpp
@@ -1,90 +1,91 @@
 #include "Bureaucrat.hpp"
-//TODO:
-//1.  operator =
-//2. Bureaucrat(Bureaucrat)
-//3. Makefile
+#include "Form.hpp"
+
 
 int main()
 {
 	cout << "*** Valid initializations ***" << endl;
 	try
 	{
-		Bureaucrat b1("B1", 1);
-		cout << b1 << endl;
+		Bureaucrat b1("B1", 100);
+		Form		f1("F1", 50, 60);
+		cout << f1 << endl;
+		f1.beSigned(b1);
+		cout << f1 << endl;
 	}
 	catch (std::exception &ex)
 	{
 		std::cerr << ex.what() << endl;
 	}
 
-	cout << "*** Invalid initializations ***" << endl;
-	try
-	{
-		Bureaucrat b2("B2", 0);
-		cout << b2 << endl;
-	}
-	catch (std::exception &ex)
-	{
-		std::cerr << ex.what() << endl;
-	}
-	try
-	{
-		Bureaucrat b3("B3", 150);
-		cout << b3 << endl;
-	}
-	catch (std::exception &ex)
-	{
-		std::cerr << ex.what() << endl;
-	}
-
-cout << "*** operations with Bureaucrats ***" << endl;
-	//Valid
-	try
-	{
-		cout << "Increase grade" << endl;
-		Bureaucrat b1("B1", 10);
-		b1.incrGrade();
-		cout << b1 << endl;
-	}
-	catch (std::exception &ex)
-	{
-		std::cerr << ex.what() << endl;
-	}
-	//Invalid
-	try
-	{
-		Bureaucrat b1("B1", 1);
-		b1.incrGrade();
-		cout << b1 << endl;
-	}
-	catch (std::exception &ex)
-	{
-		std::cerr << ex.what() << endl;
-	}
-
-	//Valid
-	try
-	{
-		cout << "Decrease grade" << endl;
-		Bureaucrat b1("B1", 10);
-		b1.decrGrade();
-		cout << b1 << endl;
-	}
-	catch (std::exception &ex)
-	{
-		std::cerr << ex.what() << endl;
-	}
-	//Invalid
-	try
-	{
-		Bureaucrat b1("B1", 150);
-		b1.decrGrade();
-		cout << b1 << endl;
-	}
-	catch (std::exception &ex)
-	{
-		std::cerr << ex.what() << endl;
-	}
+//	cout << "*** Invalid initializations ***" << endl;
+//	try
+//	{
+//		Bureaucrat b2("B2", 0);
+//		cout << b2 << endl;
+//	}
+//	catch (std::exception &ex)
+//	{
+//		std::cerr << ex.what() << endl;
+//	}
+//	try
+//	{
+//		Bureaucrat b3("B3", 150);
+//		cout << b3 << endl;
+//	}
+//	catch (std::exception &ex)
+//	{
+//		std::cerr << ex.what() << endl;
+//	}
+//
+//cout << "*** operations with Bureaucrats ***" << endl;
+//	//Valid
+//	try
+//	{
+//		cout << "Increase grade" << endl;
+//		Bureaucrat b1("B1", 10);
+//		b1.incrGrade();
+//		cout << b1 << endl;
+//	}
+//	catch (std::exception &ex)
+//	{
+//		std::cerr << ex.what() << endl;
+//	}
+//	//Invalid
+//	try
+//	{
+//		Bureaucrat b1("B1", 1);
+//		b1.incrGrade();
+//		cout << b1 << endl;
+//	}
+//	catch (std::exception &ex)
+//	{
+//		std::cerr << ex.what() << endl;
+//	}
+//
+//	//Valid
+//	try
+//	{
+//		cout << "Decrease grade" << endl;
+//		Bureaucrat b1("B1", 10);
+//		b1.decrGrade();
+//		cout << b1 << endl;
+//	}
+//	catch (std::exception &ex)
+//	{
+//		std::cerr << ex.what() << endl;
+//	}
+//	//Invalid
+//	try
+//	{
+//		Bureaucrat b1("B1", 150);
+//		b1.decrGrade();
+//		cout << b1 << endl;
+//	}
+//	catch (std::exception &ex)
+//	{
+//		std::cerr << ex.what() << endl;
+//	}
 
 
 
