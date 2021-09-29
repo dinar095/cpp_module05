45d74be849e4a456710c96ad3890c3aa997f316d
dinar095 <dinar095@hotmail.com>
2021-09-29 08:51:39 +0300

    ex03 in progr

---
+++ ex02/RobotomyRequestForm.cpp
@@ -0,0 +1,5 @@
+//
+// Created by dinar on 29.09.2021.
+//
+
+#include "RobotomyRequestForm.hpp"
---
+++ ex02/RobotomyRequestForm.hpp
@@ -0,0 +1,13 @@
+#ifndef CPP_MODULE04_ROBOTOMYREQUESTFORM_HPP
+#define CPP_MODULE04_ROBOTOMYREQUESTFORM_HPP
+#include "Form.hpp"
+
+class RobotomyRequestForm : public Form
+{
+public:
+	RobotomyRequestForm(string const & target);
+	~RobotomyRequestForm();
+};
+
+
+#endif //CPP_MODULE04_ROBOTOMYREQUESTFORM_HPP
